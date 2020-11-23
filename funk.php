eceba17dbacb4d3025943285ed162c7220288f1c
Ndingano <43851379+Ndingano@users.noreply.github.com>
2020-11-23 03:06:56 -0800

    Add files via upload

---
+++ funk.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'connect.php';
+
+
+class funk{
+    
+    //insert the asset
+    public function insert($a,$b,$c,$d,$e,$f,$g){
+       
+        
+        $sql="INSERT INTO `checking`(`AssetTag`, `id`, `SerialNumber`, `OperatingSystem`, `RAM`, `Tower Status`, `TowerBrandName`, `MonitorStatus`, `monitorSerialNumber`, `MonitorBrandName`, `keyboard`, `mouse`, `DonatedTo`, `Name`) VALUES ($a,$b,$c,$d,$e,$f,$g,)"
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+?>
