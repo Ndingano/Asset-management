6af23993ffd362d574289e652c3465aa85fdfdfe
Ndingano <43851379+Ndingano@users.noreply.github.com>
2020-11-23 03:08:08 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>asset management</title>
+  <link rel="stylesheet" type="text/css" href="theme.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+    crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/animate/animate.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/newStyle.css">
+</head>
+
+<body  style="background-color: rgb(250, 251, 252);">
+  <!-- section for navigation bar -->
+  <nav class="navbar fixed-top navbar-white" id="navbar">
+    <ul id="rect">
+      <li>
+        <a class="active shadow-pop-tr" href="index.php">Home</a>
+      </li>
+      <li>
+        <a href="asset.php">ASSETS</a>
+      </li>
+      <li>
+        <a href="contact.asp">ADMIN</a>
+      </li>
+      <li>
+        <a href="about.asp">ABOUT</a>
+      </li>
+        <li>
+        <a href="login.php">LOGIN</a>
+      </li>
+      <li style="float:right">
+        <a href="#">
+          <img class="zenImg" src="https://www.zensar.com/sites/all/themes/new_zensar/logo.png" />
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <!--end of navigation bar section -->
+  
+
+  <!-- top section -->
+
+ 
+
+  <!--end of top section -->
+
+  <!-- section for the containers -->
+  
+  <!--end of the containers  -->
+
+  <!--cards locks -->
+
+
+
+  <!--end of cards locks -->
+
+</body>
+
+<footer class="footerBar" style="background-color: rgb(3, 30, 56);" >
+  <li>
+    <p style="padding-left:19px; padding-top: 12px; color: white;">lorrem ipsum</p>
+  </li>
+  <li>
+    <p style="padding-left:20px; padding-top: 12px; color: white;"> lorrem ipsum</p>
+  </li>
+  <div class="wow fadeIn" data-wow-duration="5s" data-wow-delay="1s">
+  <a href="#" class="fa fa-facebook" ></a>
+  <a href="#" class="fa fa-twitter"></a>
+  <a href="#" class="fa fa-linkedin"></a>
+  <a href="#" class="fa fa-instagram"></a>
+  </div>
+</footer>
+
+</html>
